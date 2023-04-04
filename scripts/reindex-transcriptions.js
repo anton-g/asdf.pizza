d9cefcafb26d12bb9680f46f5074c333287a30b0
Anton Gunnarsson <gson.anton@hotmail.com>
2023-04-04 16:29:51 +0200

    Test search indexing

---
+++ scripts/reindex-transcriptions.js
@@ -0,0 +1,56 @@
+import algolia from 'algoliasearch'
+import fs from 'fs'
+import matter from 'gray-matter'
+import { marked } from 'marked'
+import { convert } from 'html-to-text'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: './.env' })
+
+const client = algolia(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_ADMIN_KEY)
+const index = client.initIndex(process.env.ALGOLIA_INDEX_NAME)
+
+const removeDuplicateNewLines = (content) => {
+  if (content.includes('\n\n')) {
+    return removeDuplicateNewLines(content.replace(/\n\n/g, '\n'))
+  } else {
+    return content
+  }
+}
+
+const reindexTranscriptions = async () => {
+  const input = process.argv.slice(2)
+  console.log(input)
+  // const episodesFilenames = input.map((x) => x.split('.')[0])
+  // const episodes = fs
+  //   .readdirSync('./src/content/episodes')
+  //   .filter((file) => file.endsWith('.md') && episodesFilenames.includes(file.split('.')[0]))
+
+  // const episodesWithTranscriptions = episodes.map((episode) => {
+  //   const episodeContent = fs.readFileSync(`./src/content/episodes/${episode}`, 'utf8')
+  //   const episodeData = matter(episodeContent)
+  //   const transcriptionFileName = episode.replace('.md', '.json')
+  //   const transcription = JSON.parse(fs.readFileSync(`./src/transcriptions/${transcriptionFileName}`))
+  //   episodeData.transcription = transcription
+  //   episodeData.slug = episode.replace('.md', '')
+
+  //   return episodeData
+  // })
+
+  // const formattedEpisodes = episodesWithTranscriptions.map((episode) => {
+  //   return {
+  //     slug: episode.slug,
+  //     objectID: episode.data.pinecastId,
+  //     title: episode.data.title,
+  //     content: removeDuplicateNewLines(convert(marked(episode.content), { wordwrap: 9999 })),
+  //     date: episode.data.date,
+  //     number: episode.data.number,
+  //     joke: episode.data.joke,
+  //     transcription: episode.transcription,
+  //   }
+  // })
+
+  // await index.saveObjects(formattedEpisodes).catch((err) => console.error(err))
+}
+
+reindexTranscriptions()
